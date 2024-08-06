8a618a455011a111ba9e778e6eaefc06dea75050
d2djobalerts <d2djobalerts@gmail.com>
2024-08-06 13:53:49 +0530

    chnages

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './App.css'
+
+const messages = ['Learn JS','Apply Jobs','Get Income'];
+function App() {
+  
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+
+  function handlePrevious(){
+    if(step>1)
+      setStep(step-1);
+      console.log(step);
+    // alert("previous");
+  }
+  function handleNExt(){
+    if(step<3)
+    setStep(step+1);
+    console.log(step);
+    // alert("next");
+  }
+  function handleOpen(){
+    isOpen ? setIsOpen(false) : setIsOpen(true);
+  }
+
+  return (
+    <>
+      <p className='cross' onClick={handleOpen}>X</p>
+      {isOpen ?
+        <div className='steps'>
+          <div className='numbers'>
+            <div className={ `${step>=1 ? 'active' : ''}` }>1</div>
+            <div className={ `${step>=2 ? 'active' : ''}` }>2</div>
+            <div className={ `${step>=3 ? 'active' : ''}` }>3</div>
+          </div>
+          <p className='message'>step {step} : {messages[step-1]}</p>
+          <div className='buttons'>
+            <button style={ {backgroundColor:'#798ef2'} } onClick={ handlePrevious}>Previous</button>
+            <button style={ {backgroundColor:'#798ef2'} } onClick={ handleNExt}>Next</button>
+          </div>
+        </div> 
+      : 
+        ""
+      }
+    </>
+  )
+}
+
+export default App
+
